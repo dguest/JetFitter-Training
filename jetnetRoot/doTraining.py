a4bfee877cce20c84f8b694cb6e1ec34b9f3ae43
Dan Guest <dguest@cern.ch>
2011-12-14 20:22:31 +0100

    pretty sure the python bindings are working well, the trainer seems to run

--- jetnetRoot/doTraining.py
+++ jetnetRoot/doTraining.py
@@ -15,16 +15,16 @@ if len(sys.argv) > 2:
         with_ip3d = True
 
 full_ds_name = 'reduceddataset_%s_forNN.root' % input_ds
-full_path = '../../../reduceddatasets/' + full_ds_name
+full_path = '../reduceddatasets/' + full_ds_name
 class_name = 'JetFitterNN_' + input_ds
 
 pynn.trainNN(input_file = full_path, 
              output_class = class_name, 
-             n_iterations = 10000, 
+             n_iterations = 1000, 
              dilution_factor = 2, 
-             use_st = False, 
+             use_sd = False, 
              with_ip3d = with_ip3d, 
              nodes_first_layer = 10, 
-             nodes_second_layer = 9, 
-             debug = True)
+             nodes_second_layer = 4, 
+             debug = False)
              

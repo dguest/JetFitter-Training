a8442b2497bf5bf209392299e98f58302dc92b57
Dan Guest <dguest@cern.ch>
2011-12-18 19:02:47 +0100

    added test function to python bindings

--- jetnetRoot/pynn.cxx
+++ jetnetRoot/pynn.cxx
@@ -1,11 +1,12 @@
-// python wrapper for JetNet based training algorythem
+// python wrapper for JetNet based training algorithm
 // Author: Daniel Guest (dguest@cern.ch)
 #include <Python.h>
 #include "TString.h"
 
 #include "trainNN.hh"
+#include "testNN.hh"
 
-static const char* doc_string = 
+static const char* train_doc_string = 
   "run the neural net. \n"
   "Keywords:\n"
   "input_file\n"
@@ -81,23 +82,65 @@ extern "C" PyObject* train_py(PyObject *self,
   return Py_None;
 }
 
-const char* getDocString(const char* kw_list[], int n_entries){ 
-  std::string doc = "run the neural net, inputs:\n"; 
-  for (int entry_n = 0; entry_n < n_entries; entry_n++){ 
-    doc.append(kw_list[entry_n]);
-    doc.append("\n"); 
-  }
-  return doc.c_str(); 
+static const char* test_doc_string = 
+  "test the neural net. \n"
+  "Keywords:\n"
+  "input_file\n"
+  "training_file\n"
+  "dilution_factor\n"
+  "use_sd\n"
+  "with_ip3d\n"; 
+
+extern "C" PyObject* test_py(PyObject *self, 
+			     PyObject *args, 
+			     PyObject *keywds)
+{
+  const char* input_file; 
+  const char* training_file = "weights/weightMinimum.root"; 
+  int dilution_factor = 2; 
+  bool use_sd = false; 
+  bool with_ip3d = true; 
+
+  static const char *kwlist[] = {
+    "input_file",
+    "training_file", 
+    "dilution_factor",
+    "use_sd",
+    "with_ip3d",
+    NULL};
+ 
+  if (!PyArg_ParseTupleAndKeywords(args, keywds, "s|sibb", 
+				   // this function should take a const, and 
+				   // may be changed, until then we'll cast
+				   const_cast<char**>(kwlist),
+				   &training_file,
+				   &dilution_factor, 
+				   &use_sd, &with_ip3d))
+    return NULL;
+
+  testNN(input_file, 
+	 training_file,
+	 dilution_factor,
+	 use_sd,
+	 with_ip3d); 
+
+  Py_INCREF(Py_None);
+
+  return Py_None;
 }
 
 
- static PyMethodDef keywdarg_methods[] = {
+
+static PyMethodDef keywdarg_methods[] = {
   // The cast of the function is necessary since PyCFunction values
   // only take two PyObject* parameters, and keywdarg() takes
   // three.
   {"trainNN", (PyCFunction)train_py, 
    METH_VARARGS | METH_KEYWORDS,
-   doc_string},
+   train_doc_string},
+  {"testNN", (PyCFunction)test_py, 
+   METH_VARARGS | METH_KEYWORDS,
+   test_doc_string},
   {NULL, NULL, 0, NULL}   /* sentinel */
 };
 

236d6af375bbe3f21416f70b7b1203b85479163b
Dan Guest <dguest@cern.ch>
2011-12-19 21:51:48 +0100

    working on multiple outputs

--- jetnetRoot/doTraining.py
+++ jetnetRoot/doTraining.py
@@ -14,31 +14,36 @@ input_ds = 'AntiKt4TopoEMJets'
 if len(sys.argv) > 1: 
     input_ds = sys.argv[1]
 
-with_ip3d = False
-if len(sys.argv) > 2: 
-    if 'True' in sys.argv[2:]: 
-        with_ip3d = True
+# with_ip3d = False
+# if len(sys.argv) > 2: 
+#     if set(['True','ip3d']) & set(sys.argv[2:]): 
+#         print 'using ip3d'
+#         with_ip3d = True
 
-out_dir = 'weights'
-if not os.path.isdir(out_dir): 
-    os.mkdir(out_dir)
-elif glob.glob(out_dir + '/*'): 
-    sys.exit("files found in 'weights', "
-             "remove them (or the directory) to continue")
+# out_dir = 'weights'
 
 
 full_ds_name = 'reduceddataset_%s_forNN.root' % input_ds
 full_path = '../reduceddatasets/' + full_ds_name
 class_name = 'JetFitterNN_' + input_ds
 
-pynn.trainNN(input_file = full_path, 
-             output_class = class_name, 
-             n_iterations = 10000, 
-             dilution_factor = 2, 
-             use_sd = False, 
-             with_ip3d = with_ip3d, 
-             nodes_first_layer = 10, 
-             nodes_second_layer = 6, 
-             debug = False, 
-             output_dir = out_dir)
+for out_dir, ip3d_state in [('ip3d_weights',True),('no_ip3d_weights',False)]:
+
+    if not os.path.isdir(out_dir): 
+        os.mkdir(out_dir)
+    elif glob.glob(out_dir + '/*'): 
+        print "files found in %s, skipping" % out_dir
+        continue
+
+
+    pynn.trainNN(input_file = full_path, 
+                 output_class = class_name, 
+                 n_iterations = 10000, 
+                 dilution_factor = 2, 
+                 use_sd = False, 
+                 with_ip3d = ip3d_state, 
+                 nodes_first_layer = 10, 
+                 nodes_second_layer = 6, 
+                 debug = True, 
+                 output_dir = out_dir)
              

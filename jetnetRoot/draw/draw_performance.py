f8c540aaf10d61e8f35df80b020464fa94a3547a
Dan Guest <dguest@cern.ch>
2012-01-10 22:59:44 -0500

    improvements on draw routine

--- jetnetRoot/draw/draw_performance.py
+++ jetnetRoot/draw/draw_performance.py
@@ -18,11 +18,11 @@ class NoMatchError(LookupError):
             return self.problem 
 
 def sig_over_background(root_file, signal = 'charm', background = 'light', 
-                        canvas = None, pad = None, sample = 'test'): 
+                        canvas = None, pad = None, sample = 'test', 
+                        normalize = False): 
     search_string = '_%s_over_%s_' % (signal, background)
 
-    train_matches = []
-    test_matches = []
+    matches = []
     
     if isinstance(root_file,str): 
         root_file = TFile(root_file)
@@ -30,10 +30,8 @@ def sig_over_background(root_file, signal = 'charm', background = 'light',
     for key in root_file.GetListOfKeys(): 
         obj = key.ReadObj()
         if search_string in obj.GetName(): 
-            if 'train' in obj.GetName(): 
-                train_matches.append(obj)
-            elif 'test' in obj.GetName(): 
-                test_matches.append(obj)
+            if sample in obj.GetName(): 
+                matches.append(obj)
 
     t = TROOT
     color_dict = { 
@@ -45,7 +43,7 @@ def sig_over_background(root_file, signal = 'charm', background = 'light',
     labels = (
         'graph',
         '#frac{%s}{%s + %s}' % (signal,signal,background), 
-        'entries'
+        '%s entries' % sample
         )
     
     if canvas is None: 
@@ -55,21 +53,25 @@ def sig_over_background(root_file, signal = 'charm', background = 'light',
         canvas.cd(pad)
     draw_string = ''
 
-    if not test_matches + train_matches: 
+    if not matches: 
         raise NoMatchError('ugh',signal = signal, background = background)
                            
     legend = TLegend(0.6,0.7,0.9,0.9)
     legend.SetFillStyle(0)
     legend.SetBorderSize(0)
     
-    for hist in test_matches: 
+    for hist in matches: 
 
         print 'drawing %s' % hist.GetName()
         sig_name = hist.GetName().split('_')[0]
         color = color_dict[sig_name]
         hist.SetTitle(';'.join(labels))
         hist.SetLineColor(color)
-        hist.Draw(draw_string)
+        if normalize == True: 
+            hist.DrawNormalized(draw_string)
+        else: 
+            hist.Draw(draw_string)
+
         draw_string = 'same'
 
         legend.AddEntry(hist, sig_name, 'l')
@@ -84,11 +86,15 @@ def sig_over_background(root_file, signal = 'charm', background = 'light',
     canvas.legend = legend
     return canvas
 
-def b_tag_plots(hist_file): 
-    plots = [
-        ('bottom','charm'), 
-        ('bottom','light')
-        ]
+def b_tag_plots(hist_file, normalize = False, signal = 'bottom'): 
+    plots = []
+
+    all_backgrounds = ['light','charm','bottom']
+    all_backgrounds.remove(signal)
+
+    for background in all_backgrounds: 
+        for sample in ['train','test']: 
+            plots.append( (signal, background, sample) )
 
     import AtlasStyle
     AtlasStyle.SetAtlasStyle()
@@ -98,10 +104,11 @@ def b_tag_plots(hist_file):
     canvas.Divide(2,2)
 
     results = []
-    for pad_index, (signal, background) in enumerate(plots): 
+    for pad_index, (signal, background, sample) in enumerate(plots): 
         pad = pad_index + 1
         r = sig_over_background(root_file = hist_file, signal = signal, 
                                 background = background, canvas = canvas, 
+                                sample = sample, normalize = normalize, 
                                 pad = pad)
         results.append(r)
 
@@ -113,9 +120,20 @@ if __name__ == '__main__':
 
     hist_file = 'all_plots.root'
 
-    if len(inputs) == 1 and 'all' in inputs: 
-        results = b_tag_plots(hist_file)
-        raw_input('press ENTER')
+    if len(inputs) == 1: 
+        signal = inputs[0]
+        for do_normalize in [False, True]: 
+            results = b_tag_plots(hist_file, normalize = do_normalize, 
+                                  signal = signal)
+
+            raw_input('press ENTER')
+
+            out_spec = '_normalized' if do_normalize else ''
+
+            output_name = '%s_perf_result%s' % (signal,out_spec)
+
+            for output_format in ['.pdf','.png']: 
+                results[0].Print(output_name + output_format)
 
         sys.exit(0)
 

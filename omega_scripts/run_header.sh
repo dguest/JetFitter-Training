0a2f26b89b5051ab4b9292972dee449b5604c7a2
Daniel Guest <daniel.guest@cern.ch>
2012-01-14 12:21:15 -0500

    some things to get athena loaded

--- omega_scripts/run_header.sh
+++ omega_scripts/run_header.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+
+shopt -s expand_aliases
+
+export ATLAS_LOCAL_ROOT_BASE=/home/hep/share/app/atlas/ATLASLocalRootBase
+# source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+
+alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+setupATLAS -q
+
+alias asetup='source $AtlasSetup/scripts/asetup.sh' 
+
+asetup 17.3.0,here
 
 echo 'submitted from: ' $PBS_O_WORKDIR 
 cd $PBS_O_WORKDIR
 
-export ATLAS_LOCAL_ROOT_BASE=/home/hep/share/app/atlas/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-alias asetup='source $AtlasSetup/scripts/asetup.sh' 
-
-asetup 17.3.0,here

9d5934bd4fef45f874663119d425987038a5f382
Dan Guest <dguest@cern.ch>
2012-01-22 16:08:39 +0100

    python executables now call 2.6

--- createDirectories.py
+++ createDirectories.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.6
 
 from jetnet import dirs
 import ConfigParser, glob, os, sys
--- jetnetRoot/doTraining.py
+++ jetnetRoot/doTraining.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python2.6
 # Author: Daniel Guest (dguest@cern.ch)
 
 import sys, dl, os, glob

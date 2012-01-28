2172c09d66fed4f236bf14ea23c2ab7fb8ee8a1a
Dan Guest <dguest@cern.ch>
2012-01-28 19:46:21 +0100

    lots of work toward making python bindings work for dataset prep, now compiles, still need to modify giacintos code to work with a vector of files

---
+++ preparedatasets/src/pyprep.cxx
@@ -0,0 +1,96 @@
+// python wrapper for neural net training set preperation 
+// Author: Daniel Guest (dguest@cern.ch)
+#include <Python.h>
+#include <string> 
+#include <vector>
+#include <iostream>
+#include "writeNtupleAll.hh"
+
+extern "C" { 
+  
+  static const char* doc_string = 
+    "inputs: TBA\n"; 
+
+  PyObject* prep_ntuple(PyObject *self, 
+			PyObject *args, 
+			PyObject *keywds)
+  {
+    PyObject* input_file_list; 
+    bool debug = false; 
+
+    const char *kwlist[] = {
+      "input_file_list",
+      "debug", 
+      NULL};
+    
+    bool ok = PyArg_ParseTupleAndKeywords
+      (args, keywds, "O|b", 
+       // this function should take a const, and 
+       // may be changed, until then we'll cast
+       const_cast<char**>(kwlist),
+       &input_file_list,
+       &debug); 
+
+    if (!ok) return NULL;
+    
+    ok = PyList_Check(input_file_list); 
+    if (!ok) { 
+      PyErr_SetString(PyExc_TypeError,"first arg should be a list of files"); 
+      return NULL; 
+    }
+    
+    std::vector<std::string> files; 
+    int n_files = PyList_Size(input_file_list); 
+    for (int i = 0; i < n_files; i++){ 
+      PyObject* the_file = PyList_GetItem(input_file_list, i); 
+      if (!PyString_Check(the_file)){ 
+	PyErr_SetString(PyExc_TypeError,
+			"found non-string in file list"); 
+	return NULL; 
+      }
+
+      std::string the_string = PyString_AsString(the_file); 
+      files.push_back(the_string); 
+    }
+
+
+    if (debug){ 
+      std::cout << "files: " << std::endl;
+      for (std::vector<std::string>::const_iterator itr = files.begin(); 
+	   itr != files.end(); 
+	   itr++){ 
+	std::cout << *itr << std::endl;
+      }
+    }
+
+    else{ 
+
+      // ****** WORK DO HERE **********
+      // writeAllNtuples(input_file_1, 
+      // 		      input_file_2, 
+      // 		      input_file_3, 
+      // 		      input_file_4);
+    }
+
+    Py_INCREF(Py_None);
+
+    return Py_None;
+  }
+
+
+  static PyMethodDef keywdarg_methods[] = {
+    // The cast of the function is necessary since PyCFunction values
+    // only take two PyObject* parameters, and keywdarg() takes
+    // three.
+    {"prep_ntuple", (PyCFunction)prep_ntuple, 
+     METH_VARARGS | METH_KEYWORDS,
+     doc_string},
+    {NULL, NULL, 0, NULL}   /* sentinel */
+  };
+
+  PyMODINIT_FUNC initpyprep(void)
+  {
+    Py_InitModule("pyprep", keywdarg_methods);
+  }
+
+}

7369550d5f640002a60f45b5686d47e534ef1127
Dan Guest <dguest@cern.ch>
2012-01-31 17:22:12 +0100

    remove root macro files

--- preparedatasets/src/prepareAllNtuplesForLikelihood.cxx
+++
@@ -1,9 +0,0 @@
-{
-gROOT->ProcessLine(".L readJFBTagAna.C+");
-gROOT->ProcessLine(".L readBaseBTagAnaTree.C+");
-gROOT->ProcessLine(".L writeNtuple_Official.cxx+");
-gROOT->ProcessLine(".L writeNtupleAll.C+");
-
-writeAllNtuples("../datasets/all.root",false);
-
-}

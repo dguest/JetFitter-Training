6d053d34b3467afb2e2cdb0ec297d726a39a6649
Dan Guest <dguest@cern.ch>
2012-02-06 11:17:07 -0500

    perf plot stuff for batch

--- testingMacros/macros/make_performance_plots.py
+++ testingMacros/macros/make_performance_plots.py
@@ -3,14 +3,9 @@ import AtlasStyle
 from jetnet.perf import rejection as re
 import sys, os
 
-if __name__ == '__main__': 
-    
-    if len(sys.argv) != 2: 
-        sys.exit('usage: %s <testing ntuple>' % 
-                 os.path.basename(sys.argv[0]))
 
+def make_performance_plots(ntuple_file_name, do_batch = True): 
     from os.path import splitext, basename, dirname
-    ntuple_file_name = sys.argv[1]
     save_dir = '%splots_%s' % (dirname(ntuple_file_name), 
                              splitext(ntuple_file_name)[0])
 
@@ -18,8 +13,8 @@ if __name__ == '__main__':
     all_canvas = re.make_plots_from(ntuple_file_name)
 
     
-
-    raw_input('press enter')
+    if not do_batch: 
+        raw_input('press enter')
     
     formats = ['.pdf','.png']
     if save_dir: 
@@ -34,3 +29,27 @@ if __name__ == '__main__':
                 plot.Print(fullpath)
 
 
+if __name__ == '__main__': 
+    
+    if len(sys.argv) != 2: 
+        sys.exit('usage: %s <testing ntuple>' % 
+                 os.path.basename(sys.argv[0]))
+
+    file_name = sys.argv[1]
+    if file_name.split('.')[-1] == 'root': 
+        make_performance_plots(ntuple_file_name)
+        sys.exit(0)
+        
+    files = []
+    with open(file_name) as the_list: 
+        for line in the_list: 
+            files.append(line.strip())
+
+    try: 
+        array_id = int(os.environ['PBS_ARRAYID'])
+        the_file = files[array_id]
+        print 'making plots for %s' % the_file
+        make_performance_plots(the_file)
+    except KeyError: 
+        for the_file in files: 
+            make_performance_plots(the_file)

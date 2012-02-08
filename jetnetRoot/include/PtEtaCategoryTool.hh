8247f6008bdb1e83fc1596703f9a5dac8eb0e4fa
Dan Guest <dguest@cern.ch>
2012-02-08 16:59:56 +0100

    fixed makefiles, should resolve deps properly now (some tools are double-compiled, thouhg)

--- jetnetRoot/include/PtEtaCategoryTool.hh
+++ jetnetRoot/include/PtEtaCategoryTool.hh
@@ -1,1 +1,30 @@
-../../preparedatasets/src/PtEtaCategoryTool.hh
+#ifndef PT_ETA_CATEGORY_TOOL_H
+#define PT_ETA_CATEGORY_TOOL_H
+
+#include <map>
+#include <vector>
+#include <ostream>
+
+class CategoryRange
+{
+public: 
+  CategoryRange(double min, double max); 
+  CategoryRange(double one_point); 
+  bool operator<(const CategoryRange& r) const ; 
+  friend std::ostream& operator<<(std::ostream&, const CategoryRange&); 
+private: 
+  double _min; 
+  double _max; 
+}; 
+
+class CategoryMap: public std::map<CategoryRange,int> 
+{ 
+public: 
+  CategoryMap(const std::vector<double>& v); 
+  int get_category(double value); 
+
+}; 
+
+std::ostream& operator<<(std::ostream& out, const CategoryRange& range); 
+
+#endif //PT_ETA_CATEGORY_TOOL_H
--- jetnetRoot/src/PtEtaCategoryTool.cxx
+++ jetnetRoot/src/PtEtaCategoryTool.cxx
@@ -1,1 +1,56 @@
-../../preparedatasets/src/PtEtaCategoryTool.cxx
+#include "PtEtaCategoryTool.hh"
+#include <map> 
+#include <vector>
+#include <cassert>
+#include <utility>
+#include <cmath>
+
+
+CategoryMap::CategoryMap(const std::vector<double>& v)
+{ 
+  double low = -INFINITY; 
+  double high = low; 
+  for (size_t category = 0; category < v.size(); category++){ 
+    low = high;
+    high = v.at(category); 
+    CategoryRange the_range(low,high); 
+    this->insert(std::make_pair(the_range, category)); 
+  }
+
+  CategoryRange end_range(high, INFINITY); 
+  this->insert( std::make_pair(end_range,v.size())); 
+  
+}
+
+std::ostream& operator<<(std::ostream& out, const CategoryRange& range)
+{
+  out << range._min << " -- " << range._max; 
+  return out; 
+} 
+
+
+int CategoryMap::get_category(double value)
+{
+  CategoryRange r(value,value);// + fabs(value*1e-5)); 
+  std::map<CategoryRange,int>::iterator loc = this->find(r); 
+  if (loc == this->end())
+    return -1; 
+  else return loc->second; 
+}
+
+CategoryRange::CategoryRange(double min, double max): 
+  _min(min), 
+  _max(max)
+{ 
+  assert(min <= max); 
+}
+
+
+bool CategoryRange::operator<(const CategoryRange& r) const
+{
+  if (this->_max <= r._max){ 
+    if (this->_min < r._min) 
+      return true; 
+  }
+  return false; 
+}

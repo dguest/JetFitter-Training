d36f0497d8d986636bd004f40e3125dc469ad988
Dan Guest <dguest@cern.ch>
2012-02-21 13:42:33 +0100

    profile now cuts on jet truth type

--- python/jetnet/profile.py
+++ python/jetnet/profile.py
@@ -39,21 +39,45 @@ def profile_rds(rds_file, tree_name = 'SVTree'):
         leaf_ranges[leaf] = (min(v), max(v))
 
     gROOT.cd()
+
+    class FilterHist(TH1D): 
+        def __init__(self, name, n_bins, min_val, max_val, 
+                     fill_leaf, req_leaf = None): 
+            super(FilterHist,self).__init__(
+                name, name, n_bins, min_val, max_val ) 
+
+            self.fill_leaf = fill_leaf
+            self.req_leaf = req_leaf
+ 
+        def fill(self,entry): 
+            if self.req_leaf: 
+                if getattr(entry, self.req_leaf) == 0: 
+                    return None
+
+            self.Fill( getattr(entry, self.fill_leaf))
+        
     hists = {}
-    for name, (min_val, max_val) in leaf_ranges.iteritems(): 
-        if leaf_types[name] == 'Int_t': 
-            n_bins = int(max_val - min_val) 
-        else: 
-            n_bins = 100
-        hists[name] = TH1D(name,name,n_bins, min_val, max_val)
+    for tag in ['bottom','charm','light', None]: 
+        for name, (min_val, max_val) in leaf_ranges.iteritems(): 
+            if leaf_types[name] == 'Int_t': 
+                max_val += 1
+                n_bins = int(max_val - min_val)
+            else: 
+                n_bins = 100
+                
+            hist_name = '%s_%s' % ( name , tag) if tag else name
+            hists[hist_name] = FilterHist(hist_name,n_bins, min_val, max_val, 
+                                          fill_leaf = name, 
+                                          req_leaf = tag)
 
     for n, entry in enumerate(sv_tree): 
         if n % 10000 == 0: 
             print '%i entries processed' % n
 
+        if n > 30000: break 
+
         for leaf, hist in hists.items(): 
-            value = getattr(entry,leaf)
-            hist.Fill(value)
+            hist.fill(entry)
     
     return hists
         

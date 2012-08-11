615c2da3ef85718fbc15fc22a84394d467d22c18
Dan Guest <dguest@cern.ch>
2012-08-11 20:22:40 +0200

    lots of moving code around in rejrej

--- python/jetnet/draw/rejrej.py
+++ python/jetnet/draw/rejrej.py
@@ -11,8 +11,10 @@ routine to draw b-rejection vs c- or l-rejection plots
 
 _tags = ['bottom','charm','light']
 
-def _get_bins(file_name, hist_name): 
+def _get_bins(file_name, hist_name, message = ''): 
     
+    if message: 
+        print message
     from ROOT import TFile
     root_file = TFile(file_name)
     
@@ -67,7 +69,9 @@ def _get_binner(max_val, n_bins = 100):
     return binner
 
 def _build_hists(input_file, cache_file = 'rejrej_cache.root', 
-                 plot_list = 'rejrej_plots.txt', bins = 600): 
+                 plot_list = '', bins = 600): 
+    if not plot_list: 
+        plot_list = cache_file.split('.')[0] + '.txt'
     if not os.path.isfile(cache_file): 
         out_file, hists = pro2d(
             in_file = input_file, 
@@ -104,11 +108,39 @@ def _build_hists(input_file, cache_file = 'rejrej_cache.root',
 
     return out_file, hists
 
-def run(in_ntuple = 'perf_ntuple.root', bins = 600): 
-    """
-    run on in_ntuple, produce bins^2 cuts
-    """
-    cache_file = 'rejrej_cache.root'
+def _get_cuts_cache(cuts_pkl, tagged_hists, root_hists): 
+
+    cache_dict = {
+        'cuts':{}, 
+        'rej':{}, 
+        }
+
+    integrals = cache_dict['cuts']
+
+    if not os.path.isfile(cuts_pkl): 
+        for h in tagged_hists: 
+            integrals[h] = _get_integrals_fast(
+                _get_bins(root_hists, h, message = 'getting bins for' + h), 
+                verbose_string = 'integrating ' + h)
+        with open(cuts_pkl,'w') as pkl: 
+            cPickle.dump(cache_dict, pkl)
+    else: 
+        sys.stdout.write('reading pickle...')
+        sys.stdout.flush()
+        with open(cuts_pkl) as pkl: 
+            cache_dict = cPickle.load(pkl)
+        sys.stdout.write('done\n')
+
+    return cache_dict
+
+
+
+def build_rej_plots(in_ntuple, bins): 
+    cache_dir = 'cache'
+    if not os.path.isdir(cache_dir): 
+        os.mkdir(cache_dir)
+
+    cache_file = os.path.join(cache_dir,'wtdist_cache.root')
     cache_file, hist_names = _build_hists(in_ntuple, 
                                           cache_file = cache_file, 
                                           bins = bins)
@@ -116,34 +148,43 @@ def run(in_ntuple = 'perf_ntuple.root', bins = 600):
     if not os.path.isfile(cache_file): 
         sys.exit('vuf')
 
-    tagged_hists = [h for h in hist_names if h.split('_')[-1] in _tags]
-
-    cache_dict = {
-        'cuts':{}, 
-        }
-
-    integrals = cache_dict['cuts']
-
-    int_pickle = 'cuts_cache.pkl'
-    if not os.path.isfile(int_pickle): 
-        for h in tagged_hists: 
-            integrals[h] = _get_integrals_fast(
-                _get_bins(cache_file, h), 
-                verbose_string = h + '\t {:>7} of {} rows complete')
-        with open(int_pickle,'w') as pkl: 
-            cPickle.dump(cache_dict, pkl)
+    rej_pickle = os.path.join(cache_dir,'rej_cache.pkl')
+    if os.path.isfile(rej_pickle): 
+        with open(rej_pickle) as pkl: 
+            rej_plots = cPickle.load(pkl)
     else: 
-        sys.stdout.write('reading pickle...')
-        sys.stdout.flush()
-        with open(int_pickle) as pkl: 
-            cache_dict = cPickle.load(pkl)
-        sys.stdout.write('done\n')
+        rej_plots = {}
 
-    p = _build_plots_from_integrals
-    p(cache_dict, tagger = 'COMBNN_SVPlus'  , use_contour = True)
-    p(cache_dict, tagger = 'JetFitterCOMBNN', use_contour = True)
-    p(cache_dict, tagger = 'COMBNN_SVPlus'  , use_contour = False)
-    p(cache_dict, tagger = 'JetFitterCOMBNN', use_contour = False)
+    taggers = ['COMBNN_SVPlus','JetFitterCOMBNN']
+    missing_taggers = [t for t in taggers if t not in rej_plots]
+
+    if missing_taggers: 
+        tagged_hists = [h for h in hist_names if h.split('_')[-1] in _tags]
+        cuts_pickle = os.path.join(cache_dir,'cuts_cache.pkl')
+        cache_dict = _get_cuts_cache(cuts_pickle, tagged_hists, cache_file)
+        for tagger in missing_taggers: 
+            rej_plots[tagger] = _build_plots_from_integrals(
+                cache_dict, tagger = tagger )
+        with open(rej_pickle,'w') as pkl: 
+            cPickle.dump(rej_plots, pkl)
+
+    return rej_plots
+
+def run(in_ntuple = 'perf_ntuple.root', bins = 600): 
+    """
+    run on in_ntuple, produce bins^2 cuts
+    """
+
+
+    rej_plots = build_rej_plots(in_ntuple, bins)
+
+    for tagger, flavors in rej_plots.iteritems(): 
+        for flavor, plot in flavors.iteritems(): 
+            print 'making {} {}'.format(tagger, flavor)
+            name = '{t}_{f}_contour.pdf'.format(t = tagger, f = flavor)
+            _plot_eff_array(plot, use_contour = True,  out_name = name)
+            name = '{t}_{f}.pdf'.format(t = tagger, f = flavor)
+            _plot_eff_array(plot, use_contour = True,  out_name = name)
 
 def _match_hist(hist_dict, matches): 
     match = None
@@ -180,7 +221,7 @@ def _loop_over_entries(x_bins, y_bins, used_eff, n_out_bins):
 
 def _loop_over_bins(x_bins, y_bins, used_eff, n_out_bins): 
     """
-    builds 
+    builds max efficiency at rejection in a different way, seems slower...
     """
     eff_array = np.zeros((n_out_bins,n_out_bins))
     info = '\rfinding eff at rejection, {} of {} rows... '
@@ -197,8 +238,46 @@ def _loop_over_bins(x_bins, y_bins, used_eff, n_out_bins):
     sys.stdout.write('done\n')
     return eff_array
 
-def _build_plots_from_integrals(cache_dict, tagger = 'COMBNN_SVPlus', 
-                                use_contour = True, calc_by_grid = True): 
+def _get_rejrej_array(flat_eff, flat_x, flat_y): 
+    indices = np.nonzero( (flat_eff > 0.05) & 
+                          np.isfinite(flat_x) & 
+                          np.isfinite(flat_y)
+                          )
+
+    used_x = np.log10(flat_x[indices])
+    used_y = np.log10(flat_y[indices])
+    used_eff = flat_eff[indices]
+
+    # allow 1% safety margin on max value
+    max_x = _max_noninf(used_x) * 1.0001
+    max_y = _max_noninf(used_y) * 1.0001
+
+    min_x = np.min(used_x)
+    min_y = np.min(used_y)
+
+    if min_y != 0.0 or min_x != 0.0: 
+        warnings.warn(
+            'may not work with current minx ({}) or miny ({})'.format(
+                min_x, min_y))
+
+    n_out_bins = 100
+
+    x_bin_values = np.linspace(min_x, max_x, n_out_bins)
+    x_bins = np.digitize(used_x, bins = x_bin_values) - 1 # no underflow
+
+    y_bin_values = np.linspace(min_y, max_y, n_out_bins)
+    y_bins = np.digitize(used_y, bins = y_bin_values) - 1 # no underflow
+
+
+    make_eff_array = _loop_over_entries
+
+    eff_array = make_eff_array(x_bins, y_bins, used_eff, n_out_bins)
+    
+    return eff_array, (min_x, max_x), (min_y, max_y)
+
+
+def _build_plots_from_integrals(cache_dict, tagger = 'COMBNN_SVPlus',
+                                calc_by_grid = True): 
 
     try: 
         integrals = cache_dict['cuts']
@@ -215,6 +294,7 @@ def _build_plots_from_integrals(cache_dict, tagger = 'COMBNN_SVPlus',
 
     sys.stdout.write('done\n')
 
+    out_dict = {}
 
     for signal in [f for f in _tags if f != 'light']: 
         x_flav, y_flav = [f for f in _tags if f != signal]
@@ -227,101 +307,78 @@ def _build_plots_from_integrals(cache_dict, tagger = 'COMBNN_SVPlus',
         flat_x = _match_hist(rej_dict, search_strings + [x_flav]).flatten()
         flat_y = _match_hist(rej_dict, search_strings + [y_flav]).flatten()
 
-        indices = np.nonzero( (flat_eff > 0.05) & 
-                              np.isfinite(flat_x) & 
-                              np.isfinite(flat_y)
-                              )
-
-        used_x = np.log10(flat_x[indices])
-        used_y = np.log10(flat_y[indices])
-        used_eff = flat_eff[indices]
-
-        # allow 1% safety margin on max value
-        max_x = _max_noninf(used_x) * 1.0001
-        max_y = _max_noninf(used_y) * 1.0001
-
-        min_x = np.min(used_x)
-        min_y = np.min(used_y)
-
-        if min_y != 0.0 or min_x != 0.0: 
-            warnings.warn(
-                'may not work with current minx ({}) or miny ({})'.format(
-                    min_x, min_y))
-
-        n_out_bins = 100
-
-        x_bin_values = np.linspace(min_x, max_x, n_out_bins)
-        x_bins = np.digitize(used_x, bins = x_bin_values) - 1 # no underflow
-
-        y_bin_values = np.linspace(min_y, max_y, n_out_bins)
-        y_bins = np.digitize(used_y, bins = y_bin_values) - 1 # no underflow
-
         sys.stdout.write('in {} entries for {}\n'.format(signal, tagger))
+        eff_array, x_range, y_range = _get_rejrej_array(
+            flat_eff, flat_x, flat_y)
 
-        if calc_by_grid: 
-            make_eff_array = _loop_over_entries
-        else: 
-            make_eff_array = _loop_over_bins
+        out_dict[signal] = { 
+            'eff':eff_array, 
+            'x_range': x_range, 
+            'y_range': y_range, 
+            'signal':signal, 
+            'x_bg':x_flav, 
+            'y_bg':y_flav
+            }
 
-        eff_array = make_eff_array(x_bins, y_bins, used_eff, n_out_bins)
-        
-        fig = plt.figure()
-        gs = GridSpec(20,21)
-        ax = plt.subplot(gs[:,0:-1])
-        aspect = float(max_x - min_x) / (max_y - min_y)
+    return out_dict
 
-        im = plt.imshow(
+def _plot_eff_array(array_dict, use_contour = True, 
+                    out_name = 'rejrej.pdf'):     
+
+    eff_array = array_dict['eff']
+    x_min, x_max = array_dict['x_range']
+    y_min, y_max = array_dict['y_range']
+
+    fig = plt.figure()
+    gs = GridSpec(20,21)
+    ax = plt.subplot(gs[:,0:-1])
+    aspect = float(x_max - x_min) / (y_max - y_min)
+
+    im = plt.imshow(
+        eff_array, 
+        extent = (x_min,x_max, y_max, y_min), 
+        aspect = aspect, 
+        interpolation = 'nearest', 
+        )
+
+    if use_contour: 
+        im.set_cmap('Greys')
+
+        ct = ax.contour(
             eff_array, 
-            extent = (min_x,max_x, max_y, min_y), 
+            extent = (x_min,x_max, y_min, y_max), 
             aspect = aspect, 
-            interpolation = 'nearest', 
+            linewidths = 2, 
+            levels = np.arange(0.1,1.0,0.1),
             )
-            # ).get_axes()
-        if use_contour: 
-            im.set_cmap('Greys')
 
-            ct = ax.contour(
-                eff_array, 
-                extent = (min_x,max_x, min_y, max_y), 
-                aspect = aspect, 
-                # color = 'k', 
-                linewidths = 2, 
-                levels = np.arange(0.1,1.0,0.1),
-                )
-                             
-            # plt.clabel(ct,fmt='%1.1f', inline=1, fontsize=10)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.invert_yaxis()
 
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.invert_yaxis()
-
-        cb_ax = plt.subplot(gs[:,-1])
-        plt.subplots_adjust(left = 0.25) # FIXME: use OO call here
-        # cb = plt.colorbar()
-        cb = Colorbar(ax = cb_ax, mappable = im)
-        cb.set_label('{} efficiency'.format(signal))
-        if use_contour: cb.add_lines(ct)
-        position = ax.get_position()
-        new_aspect = ax.get_aspect()
-        # ax.set_position(position)
+    cb_ax = plt.subplot(gs[:,-1])
+    plt.subplots_adjust(left = 0.25) # FIXME: use OO call here
+    # cb = plt.colorbar()
+    cb = Colorbar(ax = cb_ax, mappable = im)
+    cb.set_label('{} efficiency'.format(array_dict['signal']))
+    if use_contour: cb.add_lines(ct)
+    position = ax.get_position()
+    new_aspect = ax.get_aspect()
+    # ax.set_position(position)
         
 
-        ax_log = fig.add_subplot(111, frameon = False)
-        ax_log.set_xscale('log')
-        ax_log.set_yscale('log')
-        ax_log.axis((10**min_x, 10**max_x, 10**min_y, 10**max_y))
-        ax_log.set_aspect(aspect)
-        ax_log.set_xlabel('{} rejection'.format(x_flav))
-        ax_log.set_ylabel('{} rejection'.format(y_flav))
-        ax_log.grid(True)
+    ax_log = fig.add_subplot(111, frameon = False)
+    ax_log.set_xscale('log')
+    ax_log.set_yscale('log')
+    ax_log.axis((10**x_min, 10**x_max, 10**y_min, 10**y_max))
+    ax_log.set_aspect(aspect)
+    ax_log.set_xlabel('{} rejection'.format(array_dict['x_bg']))
+    ax_log.set_ylabel('{} rejection'.format(array_dict['y_bg']))
+    ax_log.grid(True)
 
 
-        ax_log.set_aspect(new_aspect)
-        ax_log.set_position(position)
-        
-        name = '{}_{}'.format(tagger,signal)
-        if use_contour: 
-            name = '{}_{}_contour'.format(tagger,signal)
+    ax_log.set_aspect(new_aspect)
+    ax_log.set_position(position)
             
-        plt.savefig(name + '.pdf', bbox_inches = 'tight')
-        plt.close()
+    plt.savefig(out_name, bbox_inches = 'tight')
+    plt.close()

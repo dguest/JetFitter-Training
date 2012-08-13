965d3c8035f3609c64cd0a82419e9739626fb1d9
Dan Guest <dguest@cern.ch>
2012-08-13 19:15:49 +0200

    lots of plotting tweaks

--- python/jetnet/draw/rejrej.py
+++ python/jetnet/draw/rejrej.py
@@ -1,6 +1,7 @@
 import numpy as np
-import os, sys, warnings, cPickle, gzip
+import os, sys, warnings, cPickle, itertools
 import matplotlib.pyplot as plt
+import matplotlib as mp
 from matplotlib.gridspec import GridSpec
 from matplotlib.colorbar import Colorbar
 from jetnet.cxxprofile import pro2d, profile_fast
@@ -220,6 +221,20 @@ def print_rej_plots(in_ntuple='perf_ntuple.root', bins=600,
             name = '{t}_{f}.pdf'.format(t = tagger, f = flavor)
             _plot_eff_array(plot, use_contour = False,  out_name = name)
 
+def print_overlay_plots(in_ntuple='perf_ntuple.root', bins=600, 
+                        range_dict = {}): 
+    """
+    run on in_ntuple, produce bins^2 cuts
+    """
+
+    rej_plots = build_rej_plots(in_ntuple, bins, range_dict)
+
+    for signal in ['bottom','charm']: 
+        jfc = rej_plots['COMBNN_SVPlus'][signal]
+        mv1 = rej_plots['discriminatorMV1'][signal]
+        cnn = rej_plots['JetFitterCOMBNN'][signal]
+        _overlay_rejrej(jfc,mv1, out_name = 'jfc-mv1-{}.pdf'.format(signal))
+        _overlay_rejrej(jfc,cnn, out_name = 'jfc-cnn-{}.pdf'.format(signal))
 
 
 def _loop_over_entries(x_bins, y_bins, used_eff, n_out_bins): 
@@ -267,7 +282,7 @@ def _loop_over_bins(x_bins, y_bins, used_eff, n_out_bins):
     return eff_array
 
 def _get_rejrej_array(flat_eff, flat_x, flat_y, x_range=None, y_range=None): 
-    indices = np.nonzero( (flat_eff > 0.05) & 
+    indices = np.nonzero( (flat_eff > 0.005) & 
                           np.isfinite(flat_x) & 
                           np.isfinite(flat_y)
                           )
@@ -385,7 +400,8 @@ def _build_plots_from_integrals(integrals, tagger = 'COMBNN_SVPlus',
             'y_range': y_range, 
             'signal':signal, 
             'x_bg':x_flav, 
-            'y_bg':y_flav
+            'y_bg':y_flav, 
+            'tagger':tagger
             }
 
     return out_dict
@@ -444,6 +460,146 @@ def _plot_eff_array(array_dict, use_contour = True,
     ax_log.set_aspect(aspect)
     ax_log.set_xlabel('{} rejection'.format(array_dict['x_bg']))
     ax_log.set_ylabel('{} rejection'.format(array_dict['y_bg']))
+    ax_log.grid(True)
+
+
+    ax_log.set_aspect(new_aspect)
+    ax_log.set_position(position)
+            
+    plt.savefig(out_name, bbox_inches = 'tight')
+    plt.close()
+
+def _get_rel_plot_alignment(*plots): 
+    """
+    I don't really know what this returns, but it gets bigger if the plots
+    aren't aligned 
+    """
+    total_diff = 0
+    total_sum = 0
+    for ax in ['x_range','y_range']: 
+        ranges = [p[ax] for p in plots]
+        for pt in zip(*ranges): 
+            diff = (max(pt) - min(pt))**2
+            add = sum(pt)**2
+            total_diff += diff
+            total_sum += add
+    return float(total_diff) / float(total_sum)
+
+def _check_sig_bg_match(*plots): 
+    sig = plots[0]['signal']
+    bgx = plots[0]['x_bg']
+    bgy = plots[0]['y_bg']
+    sig_match = all(sig == s['signal'] for s in plots)
+    bgx_match = all(bgx == s['x_bg'] for s in plots)
+    bgy_match = all(bgy == s['y_bg'] for s in plots)
+    return sig_match and bgx_match and bgy_match
+    
+
+def _overlay_rejrej(array_one, array_two,
+                    out_name = 'rejrej.pdf', 
+                    do_rel = False, do_contour = False):     
+
+    diff_array = array_one['eff'] - array_two['eff']
+
+    eff_array = diff_array 
+
+    arrays = array_one, array_two
+
+    # TODO: this is a bit messy, should find a more robust way to mask out
+    # the no-entry cells
+    for a in arrays: 
+        blank_cells = np.nonzero(a['eff'] < 1e-6)
+        diff_array[blank_cells] = -1e3
+
+    if do_rel: 
+        eff_array = eff_array / array_two['eff']
+
+    for a in arrays: 
+        if not 'tagger' in a: 
+            warnings.warn('no tagger name given',stacklevel = 2)
+            a['tagger'] = 'unknown'
+
+    if _get_rel_plot_alignment(array_one, array_two) > 1e-6: 
+        ranges = [(p['x_range'], p['y_range']) for p in arrays]
+        raise ValueError("ranges {} don't match {}".format(*ranges))
+
+    if not _check_sig_bg_match(*arrays): 
+        err = 'array mismatch ---'
+        for a in arrays: 
+            err += 'sig: {signal}, bgx: {x_bg}, bgy: {y_bg} '.format(**a)
+        raise ValueError(err)
+
+    x_min, x_max = array_one['x_range']
+    y_min, y_max = array_one['y_range']
+
+    fig = plt.figure()
+    gs = GridSpec(20,21)
+    ax = plt.subplot(gs[:,0:-1])
+    aspect = float(x_max - x_min) / (y_max - y_min)
+
+    # cmap = mp.colors.Colormap('rainbow')
+
+    plt_min = np.min(eff_array[np.nonzero(eff_array > -2)])
+    plt_max = np.max(eff_array[np.nonzero(eff_array > -2)]) 
+
+    print 'plot range: {: .4f}--{:.4f}'.format(plt_min, plt_max)
+
+    im = ax.imshow(
+        eff_array, 
+        origin = 'upper', 
+        extent = (x_min,x_max, y_min, y_max), 
+        aspect = aspect, 
+        interpolation = 'nearest', 
+        vmin = plt_min, 
+        vmax = plt_max, 
+        # cmap = cmap, 
+        )
+
+    
+    c_lines = np.arange(0.05, plt_max, 0.05)
+    if len(c_lines) == 0: 
+        do_contour = False
+    if do_contour: 
+        print 'clines', c_lines
+        ct = ax.contour(
+            eff_array, 
+            origin = 'upper', 
+            extent = (x_min,x_max, y_min, y_max), 
+            aspect = aspect, 
+            linewidths = 2, 
+            levels = c_lines,
+            )
+
+
+    im.get_cmap().set_under(alpha=0)
+    im.get_cmap().set_over(alpha=0)
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.invert_yaxis()
+
+    cb_ax = plt.subplot(gs[:,-1])
+    plt.subplots_adjust(left = 0.25) # FIXME: use OO call here
+    # cb = plt.colorbar()
+    cb = Colorbar(ax = cb_ax, mappable = im)
+    taggers = [x['tagger'] for x in arrays]
+    sig_label = array_one['signal']
+    cb.set_label('{}-{} {s} efficiency'.format(*taggers, s = sig_label ))
+    if do_contour: 
+        cb.add_lines(ct)
+
+    position = ax.get_position()
+    new_aspect = ax.get_aspect()
+    # ax.set_position(position)
+        
+
+    ax_log = fig.add_subplot(111, frameon = False)
+    ax_log.set_xscale('log')
+    ax_log.set_yscale('log')
+    ax_log.axis((10**x_min, 10**x_max, 10**y_min, 10**y_max))
+    ax_log.set_aspect(aspect)
+    ax_log.set_xlabel('{} rejection'.format(array_one['x_bg']))
+    ax_log.set_ylabel('{} rejection'.format(array_one['y_bg']))
     ax_log.grid(True)
 
 
